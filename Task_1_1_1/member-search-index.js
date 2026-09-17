b1e31a591c82cab4baf0bfdb0c235ae359000e15
KirillChepkin <KirillChepkin@users.noreply.github.com>
2026-09-17 02:25:50 +0000

    Deploying to gh-pages from @ KirillChepkin/OOP@1173657e77a2c0bec211096c1c23f3ee70eb40f2 🚀

--- Task_1_1_1/member-search-index.js
+++ Task_1_1_1/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"org.example","c":"Main","l":"heapsort(int[])"},{"p":"org.example","c":"Main","l":"Main()","u":"%3Cinit%3E()"}];updateSearchResults();
+memberSearchIndex = [{"p":"org.example","c":"Main","l":"heapsort(int[])"},{"p":"org.example","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"org.example","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
