c75e7fc211abed14a2660351b533c7a8b8db9a35
Kirill Chepkin <k.chepkin@g.nsu.ru>
2026-09-18 13:24:23 +0700

    wrote a card class

---
+++ Task_1_1_2/src/main/java/org/example/Card.java
@@ -0,0 +1,55 @@
+package org.example;
+
+/**
+ * This class represents a card in the game.
+ * A card is hidden by default and can be revealed via the method.
+ * A card has a default value according to its name.
+ * But this value can be reset in case of an ace (this.value == 11).
+ */
+public class Card {
+    public static String defaultName;
+    public static int defaultValue;
+    public String name;
+    public int value = 0;
+    public boolean isAce;
+    private boolean revealed = false;
+
+    public static void setDefault(String name, int value) {
+        defaultName = name;
+        defaultValue = value;
+    }
+
+    public Card(String name, int value, boolean isAce) {
+        this.name = name;
+        this.value = value;
+        this.isAce = isAce;
+    }
+
+    public void reveal() {
+        this.revealed = true;
+    }
+
+    /**
+     * sets card's value to 1 if it is an ace and currently revealed.
+     *
+     */
+    public void resetAceValue() {
+        if (this.revealed && this.isAce) {
+            this.value = 1;
+        }
+    }
+
+    public String getName() {
+        if (!this.revealed) {
+            return defaultName;
+        }
+        return this.name;
+    }
+
+    public int getValue() {
+        if (!this.revealed) {
+            return defaultValue;
+        }
+        return this.value;
+    }
+}
---
+++ Task_1_1_2/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/*
+ * The algorithm is:
+ * 1) create a deck
+ * 2) shuffle the deck
+ * 3) create each side (player and dealer).
+ * 4) hand each one two cards
+ * 5) reveal appropriate cards
+ */
+public class Main {
+    static void main() {
+
+    }
+}
